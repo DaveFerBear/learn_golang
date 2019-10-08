605867bea07363aa48f309ca82968970a3d9f17a
David Ferris <ferris@uber.com>
2019-10-08 14:29:38 -0700

    Week 2 files

--- week2/findian.go
+++ week2/findian.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-  "fmt",
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-  var input string
-  fmt.Scan(&input)
-  if strings.Index(input, "a") != 0 {
-    fmt.Println("Not Found!)
-  }
-}
+	consoleReader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a string \n")
 
+	input, _ := consoleReader.ReadString('\n')
+
+	lowerInput := strings.ToLower(strings.TrimSpace(input))
+
+	a := strings.HasPrefix(lowerInput, "i")
+	b := strings.HasSuffix(lowerInput, "n")
+	c := strings.Index(lowerInput, "a") > 0
+
+	fmt.Println("%t %t %t", a,b,c)
+
+	if a && b && c {
+		fmt.Println("Found!")
+	} else {
+		fmt.Println("Not Found!")
+	}
+}

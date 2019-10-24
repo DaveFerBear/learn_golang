15f9f179dc9baac433b6d12c2c5dd88274528e74
David Ferris <ferris@uber.com>
2019-10-24 10:30:05 -0700

    fix bubble sort al

--- 2-functions-methods-interfaces/week1/bubblesort.go
+++ 2-functions-methods-interfaces/week1/bubblesort.go
@@ -4,40 +4,39 @@ import (
 	"fmt"
 )
 
-
-func swap(sli []int, i int, j int) {
-    sli[i], sli[j] = sli[j], sli[i]
+func Swap(s []int, i int) {
+	s[i+1], s[i] = s[i], s[i+1]
 }
-
-func bubbleSort(sli []int) {
-    for i, a := range sli {
-        for j, b := range sli {
-            if a < b {
-                swap(sli, i, j)
-            }
-        }
-    }
+func BubbleSort(s []int) {
+	n := len(s)
+	for j := 0; j < n-1; j++ {
+		for i := 0; i < n-1; i++ {
+			if s[i] > s[i+1] {
+				Swap(s, i)
+			}
+		}
+	}
 }
 
 func main() {
-    SIZE := 10
+	SIZE := 10
 
-    // Make a slice
-    arr := make([]int, SIZE)
+	// Make a slice
+	arr := make([]int, SIZE)
 
-    // Read numbers into array
-    var i int
-    for d:=SIZE; d>0; d-- {
-        fmt.Printf("Enter %d more integers: ", d)
-	    fmt.Scanf("%d", &i)
-	    arr[SIZE-d] = i
-    }
+	// Read numbers into array
+	var i int
+	for d := SIZE; d > 0; d-- {
+		fmt.Printf("Enter %d more integers: ", d)
+		fmt.Scanf("%d", &i)
+		arr[SIZE-d] = i
+	}
 
-    fmt.Print("The (unsorted) numbers you entered: ")
-    fmt.Println(arr)
+	fmt.Print("The (unsorted) numbers you entered: ")
+	fmt.Println(arr)
 
-    bubbleSort(arr)
+	BubbleSort(arr)
 
-    fmt.Print("Sorted: ")
-    fmt.Println(arr)
+	fmt.Print("Sorted: ")
+	fmt.Println(arr)
 }
